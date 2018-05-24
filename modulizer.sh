dc43376a1576d057ea7e1930e26f6a0128906527
Paul Varache <perso@paulvarache.ninja>
2018-05-24 17:59:04 +0100

    Module

---
+++ modulizer.sh
@@ -0,0 +1,5 @@
+modulizer --out . \
+--npm-name @kano/kwc-badge \
+--npm-version 3.0.0-beta.1 \
+--dependency-mapping kwc-paper-tooltip,@kano/kwc-paper-tooltip,3.0.0-beta.1 \
+--dependency-mapping web-components,@kano/web-components,3.0.0-beta.1
